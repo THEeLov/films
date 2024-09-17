00ee73f4d4d066227cfca64fb2e8c2a6077925c2
Filip Kozik <536410@mail.muni.cz>
2024-09-17 21:11:04 +0200

    fix: remove decorator

--- src/decorators/Decorator.tsx
+++ src/decorators/Decorator.tsx
@@ -1,21 +1,11 @@
 import { Box } from "@mui/material";
 
 const Decorator = () => {
-  const boxes = Array.from({ length: 20 });
+
 
   return (
     <>
-      {boxes.map((_, index) => {
-        return (
-          <Box
-            key={index}
-            sx={{
-              ...sizeStyle,
-              ...positionStyle,
-            }}
-          />
-        );
-      })}
+      
     </>
   );
 };
