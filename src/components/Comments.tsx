19b13b13c47d9bc285b1450b3b71cee08d063df6
Filip Kozik <536410@mail.muni.cz>
2024-09-19 19:03:02 +0200

    refactor: featch comments in useComments hook

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,43 +1,23 @@
-import { useEffect, useState } from "react";
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { store } from "../config/firebase";
 import CommentCard from "./cards/CommentCard";
 import { Box } from "@mui/material";
+import { useComments } from "../hooks/useComments";
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Comments = ({ movieId }: { movieId: string }) => {
-  const [comments, setComments] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const commentsRef = collection(store, `movies/${movieId}/comments`);
-    const q = query(commentsRef, orderBy("date", "desc"));
+  // Hook for getting all the comments for a movie
+  const { comments, loading, error } = useComments(movieId);
 
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      try {
-        const fetchedComments = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setComments(fetchedComments);
-      } catch (error) {
-        setError("Failed to fetch comments.");
-        console.error("Error fetching comments: ", error);
-      } finally {
-        setLoading(false);
-      }
-    });
+  if (loading) {
+    return <CircularProgress/>
+  }
 
-    return () => unsubscribe();
-  }, [movieId]);
-
+  if (error) {
+    return <Box component="div">{error}</Box>;
+  }
   return (
     <Box component="div">
-      {loading ? (
-        <Box component="div">Loading comments...</Box>
-      ) : error ? (
-        <Box component="div">{error}</Box>
-      ) : comments && comments.length === 0 ? (
+      {comments.length === 0 ? (
         <Box component="div">No comments yet.</Box>
       ) : (
         comments &&
