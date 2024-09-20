20224806aeacd8a5098fd837b31ca4eb0365394b
Filip Kozik <536410@mail.muni.cz>
2024-09-20 14:55:52 +0200

    refactor: ratingService added for updating rating of user

--- src/forms/RatingForm.tsx
+++ src/forms/RatingForm.tsx
@@ -2,16 +2,16 @@ import { Box, Rating } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ratingSchema } from "../validationSchemas/movieForms";
-import { doc, getDoc, serverTimestamp, writeBatch } from "firebase/firestore";
-import { store } from "../config/firebase";
 import { z } from "zod";
 import { useAuth } from "../contexts/AuthProvider";
-import { useEffect } from "react";
+import { useUserRating } from "../hooks/useUserRating";
+import { submitRating } from "../services/ratingService";
 
 type RatingSchema = z.infer<typeof ratingSchema>;
 
 const RatingForm = ({ movieId }: { movieId: string }) => {
   const { currentUser } = useAuth();
+  const { userRating } = useUserRating(movieId, currentUser);
 
   const {
     handleSubmit,
@@ -22,7 +22,7 @@ const RatingForm = ({ movieId }: { movieId: string }) => {
     resolver: zodResolver(ratingSchema),
   });
 
-  const ratingValue = watch("rating", 0);
+  const ratingValue = watch("rating", userRating);
 
   const handleRatingChange = async (_: any, newValue: number | null) => {
     if (newValue !== null) {
@@ -37,79 +37,13 @@ const RatingForm = ({ movieId }: { movieId: string }) => {
       return;
     }
 
-    const movieRef = doc(store, `movies/${movieId}`);
-    const userRatingRef = doc(
-      store,
-      `movies/${movieId}/ratings/${currentUser.uid}`
-    );
-    const movieSnapshot = await getDoc(movieRef);
-    const userRatingSnapshot = await getDoc(userRatingRef);
-
-    const batch = writeBatch(store);
-
-    if (!movieSnapshot.exists()) {
-      throw new Error("Movie does not exist.");
-    }
-
-    const movieData = movieSnapshot.data();
-
-    let newSumOfRatings = movieData.sumOfRatings;
-    let newTotalRatings = movieData.totalRatings;
-
-    // If user has already rated the movie, update the rating
-    if (userRatingSnapshot.exists()) {
-      const userRatingData = userRatingSnapshot.data();
-      const oldRating = userRatingData.rating;
-      newSumOfRatings = movieData.sumOfRatings - oldRating + data.rating;
-    } else {
-      // If it's a new rating, increase the totalRatings
-      newTotalRatings = movieData.totalRatings + 1;
-      newSumOfRatings = movieData.sumOfRatings + data.rating;
-    }
-
-    // Update movie document with new ratings data
-    batch.update(movieRef, {
-      sumOfRatings: newSumOfRatings,
-      totalRatings: newTotalRatings,
-      averageRating: (newSumOfRatings / newTotalRatings / 5) * 100,
-    });
-
-    // Set or update the user's rating in the subcollection
-    batch.set(userRatingRef, {
-      userId: currentUser.uid,
-      rating: data.rating,
-      timestamp: serverTimestamp(),
-    });
-
     try {
-      await batch.commit();
+      await submitRating(movieId, currentUser.uid, data.rating);
     } catch (err) {
       console.error("Error updating movie and user's rating: ", err);
       alert("Failed to update rating. Please try again.");
     }
   };
-
-  useEffect(() => {
-    const fetchUserRating = async () => {
-      if (!currentUser) return;
-
-      try {
-        const userRatingRef = doc(store, `movies/${movieId}/ratings/${currentUser.uid}`);
-        const userRatingSnap = await getDoc(userRatingRef);
-
-        if (userRatingSnap.exists()) {
-          const userRatingData = userRatingSnap.data();
-          if (userRatingData && userRatingData.rating) {
-            setValue("rating", userRatingData.rating);
-          }
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchUserRating();
-  }, [currentUser, movieId, setValue]);
 
   return (
     <Box
@@ -120,6 +54,7 @@ const RatingForm = ({ movieId }: { movieId: string }) => {
       flexDirection="column"
       width="100%"
       gap="1rem"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <Rating
         name="controlled-rating"
